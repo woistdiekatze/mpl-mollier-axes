8333dca727248d29416d651dd240f166316b4783
woistdiekatze <24732797+woistdiekatze@users.noreply.github.com>
2023-01-04 00:38:05 +0100

    export projection

--- mpl_mollier_axes/axes/__init__.py
+++ mpl_mollier_axes/axes/__init__.py
@@ -1,2 +1,2 @@
-from .MollierAxes import MollierAxes  # noqa: F401
+from .MollierAxes import MollierAxes, MollierProjection  # noqa: F401
 from .SkewYAxes import SkewYAxes, SkewYProjection  # noqa: F401
