be02eab27ca9f881d1109fcfa919cba0bb96add7
woistdiekatze <24732797+woistdiekatze@users.noreply.github.com>
2023-01-04 13:12:49 +0100

    bump minor version

--- mpl_mollier_axes/__init__.py
+++ mpl_mollier_axes/__init__.py
@@ -1,3 +1,3 @@
 from .axes import MollierAxes, MollierProjection  # noqa: F401
 
-__version__ = '0.1.0'
+__version__ = '0.2.0-alpha.0'
--- tests/test_mpl_mollier_axes.py
+++ tests/test_mpl_mollier_axes.py
@@ -2,4 +2,4 @@ from mpl_mollier_axes import __version__
 
 
 def test_version():
-    assert __version__ == '0.1.0'
+    assert __version__ == '0.2.0-alpha.0'
