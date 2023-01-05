713cc3da21bddacafd780be370b55ad782a85328
woistdiekatze <24732797+woistdiekatze@users.noreply.github.com>
2023-01-05 15:35:01 +0100

    add mpl image comparison tests

--- tests/test_lines.py
+++ tests/test_lines.py
@@ -1,28 +1,39 @@
 from __future__ import annotations
 
+import matplotlib.pyplot as plt
 import numpy as np
-import pytest
 from matplotlib.lines import Line2D
+from matplotlib.testing.decorators import image_comparison
 
-import mpl_mollier_axes as mol
 import mpl_mollier_axes.lines as lines
 
 
-@pytest.fixture
-def ax(mollier_axes: mol.MollierAxes) -> mol.MollierAxes:
-    mollier_axes.draw_saturation_line()
-    return mollier_axes
+def fig_ax(saturation=True):
+    fig = plt.figure()
+    ax = fig.add_subplot(projection='mollier')
+    ax.set_xlim(0, 4e-2)
+    ax.set_ylim(-1e4, 5e4)
+    ax.grid(visible=True, which='both')
+    if saturation:
+        ax.draw_saturation_line()
+    return fig, ax
 
 
-@pytest.mark.fig_filename('sat_line.png')
-def test_saturation_line(mollier_axes: mol.MollierAxes):
-    sat = mollier_axes.draw_saturation_line(color='red')
+@image_comparison(baseline_images=['sat_line'],
+                  remove_text=True,
+                  extensions=['pdf'])
+def test_saturation_line():
+    fig, ax = fig_ax(False)
+    sat = ax.draw_saturation_line(color='red')
     assert isinstance(sat, Line2D)
     assert isinstance(sat, lines.ParametricConstValueLine)
 
 
-@pytest.mark.fig_filename('rh_lines.png')
-def test_const_rh_lines(ax: mol.MollierAxes):
+@image_comparison(baseline_images=['rh_lines'],
+                  remove_text=True,
+                  extensions=['pdf'])
+def test_const_rh_lines():
+    fig, ax = fig_ax()
     rhs = ax.draw_const_rh_lines(*np.linspace(0.1, 0.9, 9), color='red')
 
     assert len(rhs) == 9
@@ -31,8 +42,11 @@ def test_const_rh_lines(ax: mol.MollierAxes):
         assert isinstance(RHL, lines.ParametricConstValueLine)
 
 
-@pytest.mark.fig_filename('tdb_lines.png')
-def test_const_tdb_lines(ax: mol.MollierAxes):
+@image_comparison(baseline_images=['tdb_lines'],
+                  remove_text=True,
+                  extensions=['pdf'])
+def test_const_tdb_lines():
+    fig, ax = fig_ax()
     tdbs = ax.draw_const_tdb_lines(*np.linspace(-5, 50, 12), color='red')
 
     assert len(tdbs) == 12
@@ -41,8 +55,11 @@ def test_const_tdb_lines(ax: mol.MollierAxes):
         assert isinstance(TDBL, lines.BoundedConstValueLine)
 
 
-@pytest.mark.fig_filename('rho_lines.png')
-def test_const_rho_lines(ax: mol.MollierAxes):
+@image_comparison(baseline_images=['rho_lines'],
+                  remove_text=True,
+                  extensions=['pdf'])
+def test_const_rho_lines():
+    fig, ax = fig_ax()
     rhos = ax.draw_const_density_lines(*np.linspace(1.1, 1.3, 5), color='red')
 
     assert len(rhos) == 5
@@ -51,8 +68,11 @@ def test_const_rho_lines(ax: mol.MollierAxes):
         assert isinstance(RHOL, lines.ConstValueLine)
 
 
-@pytest.mark.fig_filename('h_lines.png')
-def test_const_h_lines(ax: mol.MollierAxes):
+@image_comparison(baseline_images=['h_lines'],
+                  remove_text=True,
+                  extensions=['pdf'])
+def test_const_h_lines():
+    fig, ax = fig_ax()
     hs = ax.draw_const_h_lines(*np.linspace(1e4, 3e4, 5), color='red')
 
     assert len(hs) == 5
@@ -60,8 +80,11 @@ def test_const_h_lines(ax: mol.MollierAxes):
         assert isinstance(HL, Line2D)
 
 
-@pytest.mark.fig_filename('w_lines.png')
-def test_const_w_lines(ax: mol.MollierAxes):
+@image_comparison(baseline_images=['w_lines'],
+                  remove_text=True,
+                  extensions=['pdf'])
+def test_const_w_lines():
+    fig, ax = fig_ax()
     ws = ax.draw_const_w_lines(*np.linspace(1e-2, 3e-2, 9), color='red')
 
     assert len(ws) == 9
