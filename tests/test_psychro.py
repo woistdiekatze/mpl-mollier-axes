d701317d329cb9d30d1fa191efa962a0617fe141
Nikolaus Alessandria <24732797+woistdiekatze@users.noreply.github.com>
2026-04-17 18:05:16 +0200

    flake8

--- tests/test_psychro.py
+++ tests/test_psychro.py
@@ -1,5 +1,7 @@
-import mpl_mollier_axes.lines.psychrometric as plines
 import numpy as np
+
+import mpl_mollier_axes.lines.psychrometric as plines
+
 
 def test_saturation_data():
     pressure = 101325.
